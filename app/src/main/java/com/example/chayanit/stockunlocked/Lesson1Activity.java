81205e3f2f1aa84a41a9ff13664570fd9b1a4882
Nicee <nnatnichab@gmail.com>
2020-02-19 16:17:01 +0700

    unlock lesson 2 button

--- app/src/main/java/com/example/chayanit/stockunlocked/Lesson1Activity.java
+++ app/src/main/java/com/example/chayanit/stockunlocked/Lesson1Activity.java
@@ -2,6 +2,8 @@ package com.example.chayanit.stockunlocked;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Lesson1Activity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class Lesson1Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson1);
+
+        Button unlockls2_bt = findViewById(R.id.unlockls2);
+
+        unlockls2_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
+
 }
