355287197cbe97a84474429efa2aec470c726285
Nicee <nnatnichab@gmail.com>
2020-02-19 14:42:55 +0700

    add button and new activity

--- app/src/main/java/com/example/chayanit/stockunlocked/TitleActivity.java
+++ app/src/main/java/com/example/chayanit/stockunlocked/TitleActivity.java
@@ -1,7 +1,10 @@
 package com.example.chayanit.stockunlocked;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TitleActivity extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class TitleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
+        Button lesson_bt = findViewById(R.id.lesson_bt);
+        Button quiz_bt = findViewById(R.id.quiz_bt);
+        Button exam_bt = findViewById(R.id.exam_bt);
+
+        lesson_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(TitleActivity.this,LessonActivity.class));
+            }
+        });
+
+        quiz_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(TitleActivity.this,QuizActivity.class));
+            }
+        });
+
+        exam_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(TitleActivity.this,ExamActivity.class));
+            }
+        });
     }
 }
