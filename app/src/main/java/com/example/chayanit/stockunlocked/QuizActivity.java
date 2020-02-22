2442ab5670e73ac502f6837153aeca893e3bca5d
Patty <pat2baker2street1@gmail.com>
2020-02-22 15:49:31 +0700

    add back button for quiz, exam, setting

--- app/src/main/java/com/example/chayanit/stockunlocked/QuizActivity.java
+++ app/src/main/java/com/example/chayanit/stockunlocked/QuizActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -14,6 +15,14 @@ public class QuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        ImageButton quiz_back = findViewById(R.id.quizback);
+        quiz_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(QuizActivity.this,TitleActivity.class));
+            }
+        });
 
         SharedPreferences shared = getSharedPreferences("my_prefs", Context.MODE_PRIVATE);
 
--- app/src/main/java/com/example/chayanit/stockunlocked/SettingActivity.java
+++ app/src/main/java/com/example/chayanit/stockunlocked/SettingActivity.java
@@ -1,11 +1,13 @@
 package com.example.chayanit.stockunlocked;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -13,6 +15,14 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        ImageButton setting_back = findViewById(R.id.settingback);
+        setting_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SettingActivity.this,TitleActivity.class));
+            }
+        });
 
         Button reset_bt = findViewById(R.id.reset);
         reset_bt.setOnClickListener(new View.OnClickListener() {
