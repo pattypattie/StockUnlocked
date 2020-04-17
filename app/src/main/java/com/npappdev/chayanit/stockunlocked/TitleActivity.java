c752e366114acb5664c0e482d083e87c6bff687f
Nicee <nnatnichab@gmail.com>
2020-04-17 22:09:47 +0700

    unlock all lesson first

--- app/src/main/java/com/npappdev/chayanit/stockunlocked/TitleActivity.java
+++ app/src/main/java/com/npappdev/chayanit/stockunlocked/TitleActivity.java
@@ -1,10 +1,13 @@
 package com.npappdev.chayanit.stockunlocked;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class TitleActivity extends AppCompatActivity {
 
@@ -15,6 +18,10 @@ public class TitleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
+
+        final SharedPreferences shared = getSharedPreferences("my_prefs", Context.MODE_PRIVATE);
+        final boolean finAll = shared.getBoolean("qz6_enable", false);
+
 
         Button lesson_bt = findViewById(R.id.lesson_bt);
         Button quiz_bt = findViewById(R.id.quiz_bt);
@@ -35,13 +42,20 @@ public class TitleActivity extends AppCompatActivity {
             }
         });
 
-        exam_bt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(TitleActivity.this,ExamActivity.class));
-            }
-        });
-        setting_bt.setOnClickListener(new View.OnClickListener() {
+            exam_bt.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+        if(finAll==false){
+            Toast.makeText(TitleActivity.this, "Unlock all lessons first!", Toast.LENGTH_SHORT).show();
+
+        } else {
+            startActivity(new Intent(TitleActivity.this, ExamActivity.class));
+        }
+                }
+            });
+
+            setting_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(TitleActivity.this, SettingActivity.class));
